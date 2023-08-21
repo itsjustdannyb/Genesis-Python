844eed79f68210605414ec65101cafeb9fcf1cf6
itsjustdannyb <danielbamilosin@gmail.com>
2023-08-22 00:22:56 +0100

    button layout add

--- Calculator app.py
+++ Calculator app.py
@@ -23,6 +23,9 @@ button_7 = Button(root, text="7", padx=40, pady=20, command=button_add)
 button_8 = Button(root, text="8", padx=40, pady=20, command=button_add)
 button_9 = Button(root, text="9", padx=40, pady=20, command=button_add)
 button_0 = Button(root, text="0", padx=40, pady=20, command=button_add)
+button_add = Button(root, text="+", padx=39, pady=20, command=button_add)
+button_equal = Button(root, text="=", padx=91, pady="20", command="button_add")
+button_clear = Button(root, text="Clear", padx=79, pady=20, command="button_add")
 
 #Display Buttons on the Screen
 
@@ -32,12 +35,15 @@ button_3.grid(row=3, column=2)
 
 button_4.grid(row=2, column=0)
 button_5.grid(row=2, column=1)
-button_6.grid(row=2, column=)
+button_6.grid(row=2, column=2)
 
-button_7.grid(row=, column=)
-button_8.grid(row=, column=)
-button_9.grid(row=, column=)
+button_7.grid(row=1, column=0)
+button_8.grid(row=1, column=1)
+button_9.grid(row=1, column=2)
 
-
+button_0.grid(row=4, column=0)
+button_clear.grid(row=4, column=3)
+button_add.grid(row=5, column=0)
+button_equal.grid(row=5,column=1)
 #prevents app from closing prematurely
 root.mainloop()
