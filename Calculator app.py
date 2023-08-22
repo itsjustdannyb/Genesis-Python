c196e786444e3da46e745d0027947560de9fe1a6
itsjustdannyb <danielbamilosin@gmail.com>
2023-08-22 14:18:48 +0100

    modified calculator app

--- Calculator app.py
+++ Calculator app.py
@@ -6,26 +6,34 @@ root.title("Calculator!")
 entry_box = Entry(root, width=50, borderwidth=3)
 entry_box.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
-#addition function
-def button_add():
+#click function
+def button_click(number):
+    # entry_box.delete(0, END)
+    current = entry_box.get()
+    entry_box.delete(0, 'end')
+    entry_box.insert(0, str(current) + str(number))
     return
+
+#clear function
+def clear_button():
+    entry_box.delete(0, 'end')
 
 #Define Buttons
 
 
-button_1 = Button(root, text="1", padx=40, pady=20, command=button_add)
-button_2 = Button(root, text="2", padx=40, pady=20, command=button_add)
-button_3 = Button(root, text="3", padx=40, pady=20, command=button_add)
-button_4 = Button(root, text="4", padx=40, pady=20, command=button_add)
-button_5 = Button(root, text="5", padx=40, pady=20, command=button_add)
-button_6 = Button(root, text="6", padx=40, pady=20, command=button_add)
-button_7 = Button(root, text="7", padx=40, pady=20, command=button_add)
-button_8 = Button(root, text="8", padx=40, pady=20, command=button_add)
-button_9 = Button(root, text="9", padx=40, pady=20, command=button_add)
-button_0 = Button(root, text="0", padx=40, pady=20, command=button_add)
-button_add = Button(root, text="+", padx=39, pady=20, command=button_add)
-button_equal = Button(root, text="=", padx=91, pady="20", command="button_add")
-button_clear = Button(root, text="Clear", padx=79, pady=20, command="button_add")
+button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
+button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
+button_3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
+button_4 = Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4))
+button_5 = Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5))
+button_6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6))
+button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
+button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
+button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
+button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
+button_add = Button(root, text="+", padx=39, pady=20, command=lambda: button_click())
+button_equal = Button(root, text="=", padx=93, pady=20, command=lambda: button_click())
+button_clear = Button(root, text="Clear", padx=84, pady=20, command=clear_button)
 
 #Display Buttons on the Screen
 
@@ -42,8 +50,8 @@ button_8.grid(row=1, column=1)
 button_9.grid(row=1, column=2)
 
 button_0.grid(row=4, column=0)
-button_clear.grid(row=4, column=3)
+button_clear.grid(row=4, column=1, columnspan=2)
 button_add.grid(row=5, column=0)
-button_equal.grid(row=5,column=1)
+button_equal.grid(row=5, column=1, columnspan=2)
 #prevents app from closing prematurely
 root.mainloop()
