e7026ebd191adf679b2735e563c87b15383aa40a
itsjustdannyb <danielbamilosin@gmail.com>
2023-08-24 01:27:13 +0100

    new tutorials, clear code

--- Views app.py
+++ Views app.py
@@ -12,25 +12,43 @@ def forward(image_number):
     global backward_btn
 
     image_label.grid_forget()
-    image_label = Label(image=image_list[image_number], width=500, height=500)
+    image_label = Label(image=image_list[image_number - 1], width=500, height=500)
+    image_label.grid(row=0, column=0, columnspan=3)
+
+    #I have no idea how this part works... shouldn't image number be 0 still??
+    if image_number == 5:
+        forward_btn = Button(root, text=">>", state=DISABLED)
+    else:
+        forward_btn = Button(root, text=">>", command=lambda: forward(image_number + 1))
+        forward_btn.grid(row=1, column=2)
+
+    backward_btn = Button(root, text="<<", command=lambda: forward(image_number - 1))
+    backward_btn.grid(row=1, column=0)
+
+    if image_number == 5:
+        forward_btn = Button(root, text=">>", state=DISABLED)
+
+    return
+
+def backward(image_number):
+    global image_label
+    global forward_btn
+    global backward_btn
+
+    mage_label.grid_forget()
+    image_label = Label(image=image_list[image_number - 1], width=500, height=500)
     image_label.grid(row=0, column=0, columnspan=3)
 
     forward_btn = Button(root, text=">>", command=lambda: forward(image_number + 1))
     forward_btn.grid(row=1, column=2)
 
-    backward_btn = Button(root, text="<<", command=lambda: forward(image_number - 1))
-    forward_btn.grid(row=1, column=0)
+    if image_number == 1:
+        backward_btn = Button(root, text="<<", command=DISABLED)
+    else:
+        backward_btn = Button(root, text="<<", command=lambda: forward(image_number - 1))
+        backward_btn.grid(row=1, column=0)
 
-    if image_number == 3:
-        forward_btn = Button(root, text=">>", state=DISABLED)
 
-
-    return
-
-def backward():
-    global image_label
-    global forward_btn
-    global backward_btn
 
     return
 
@@ -40,19 +58,19 @@ image_1 = ImageTk.PhotoImage(Image.open("images/baby panda.jpg"))
 image_2 = ImageTk.PhotoImage(Image.open("images/Naruto_watercolor.jpg"))
 image_3 = ImageTk.PhotoImage(Image.open("images/luffy_onepiece.jpg"))
 image_4 = ImageTk.PhotoImage(Image.open("images/Suka_one piece.jpg"))
-
+image_5 = ImageTk.PhotoImage(Image.open("images/Saitama.jpg"))
 
 #scrolling
-image_list = [image_1, image_2, image_3, image_4]
+image_list = [image_1, image_2, image_3, image_4, image_5]
 
 
 image_label = Label(image=image_1, width=500, height=500)
 image_label.grid(row=0, column=0, columnspan=3)
 
 #Buttons
-backward_btn = Button(root, text="<<", command=backward)
+backward_btn = Button(root, text="<<", command=backward, state=DISABLED)
 exit_btn = Button(root, text="Leave", command=root.quit)
-forward_btn = Button(root, text=">>", command=lambda: forward(1))
+forward_btn = Button(root, text=">>", command=lambda: forward(2))
 
 
 backward_btn.grid(row=1, column=0)
--- basic_widgets.py
+++ basic_widgets.py
@@ -1,26 +1,38 @@
 import tkinter as tk
 from tkinter import ttk
+
 #create window
 root = tk.Tk()
 root.title("Tutorial")
 root.geometry("800x500")
+
+
+#functions
+def say_hello():
+    print("Hello")
+
 
 #create widgets
 text = tk.Text(master=root)
 text.pack()
 
 #ttk label
-label = ttk.Label(root, text="This is a text")c d
+label = ttk.Label(root, text="This is a text")
 label.pack()
 
 #ttk entry
 entry = ttk.Entry(master=root)
 entry.pack()
 
+text2 = ttk.Label(master=root, text='my label')
+text2.pack()
+
 #ttk button
 btn = ttk.Button(master=root, text="Save", command=root.quit)
 btn.pack()
 
-
+# btn2 = ttk.Button(master=root, text="hello", command=say_hello)
+btn2 = ttk.Button(master=root, text="Hello", command=lambda: print("Hello"))
+btn2.pack()
 #Maintain window
 root.mainloop()
